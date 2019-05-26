5d93422b51ebdbb92f1246f402ca3fde0a9335ba
Rien Bijl <helloitsrien@gmail.com>
2019-05-26 15:34:10 +0200

    Newline at end of Lexer.php

--- src/phpmarkdown/Lexer.php
+++ src/phpmarkdown/Lexer.php
@@ -2,7 +2,28 @@
 
 namespace PHPMarkdown;
 
-class Markdown
+
+/*
+ * Lexer
+ *
+ * Prepares the text for easy use in the Formatter
+ */
+class Lexer
 {
-    
+
+    private $rawContent;
+    private $parsedContent;
+
+    /*
+     * Constructs the lexer
+     *
+     * Takes in the raw content and stores it in rawContent for later use
+     *
+     * $param string rawContent
+     */
+    public function __construct($rawContent)
+    {
+        $this->$rawContent = $rawContent;
+    }
+
 }
