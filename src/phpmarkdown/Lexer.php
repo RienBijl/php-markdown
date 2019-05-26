0c71c55919a8fd5132a443b7f4473e8c9a23255b
Rien Bijl <helloitsrien@gmail.com>
2019-05-26 18:11:42 +0200

    Added sanitization

--- src/phpmarkdown/Lexer.php
+++ src/phpmarkdown/Lexer.php
@@ -20,10 +20,30 @@ class Lexer
      * Takes in the raw content and stores it in rawContent for later use
      *
      * $param string $rawContent The unparsed content for Lexer
+     * $param boolean $sanitize Should the string be sanitized
      */
-    public function __construct($rawContent)
+    public function __construct($rawContent, $sanitize = true)
     {
+        if($sanitize)
+        {
+            $rawContent = $this->sanitize($rawContent);
+        }
         $this->rawContent = $rawContent;
+    }
+
+    /*
+     * Sanitizes the contentstring
+     *
+     * Takes in the unsanitized string and sanitizes it
+     *
+     * $param string $unsanitized The unsantized string
+     * $param boolean $sanitize Should we sanitize the String?
+     *
+     * @return string The sanitized string
+    */
+    private function sanitize($unsanitized)
+    {
+        return htmlspecialchars($unsanitized, ENT_QUOTES);
     }
 
 }
